850e97849b21f36c5c0280514713ebb43df7cb3a
JoaoJales <joaoricardocirino@gmail.com>
2026-03-02 21:07:21 -0300

    Atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,21 @@
+var soma1 = 10;
+var soma2 = 20;
+var resultadoSoma = soma1 + soma2;
+console.log(`${soma1} + ${soma2} = ${resultadoSoma}`)
+
+
+var sub1 = 10;
+var sub2 = 5;
+var resultadoSub = sub1 - sub2;
+console.log(`${sub1} - ${sub2} = ${resultadoSub}`)
+
+
+var multi1 = 10;
+var multi2 = 5;
+var resultadoMult = multi1 * multi2;
+console.log(`${multi1} x ${multi2} = ${resultadoMult}`)
+
+var div1 = 10;
+var div2 = 5;
+var resultadoDiv = div1 / div2;
+console.log(`${div1} / ${div2} = ${resultadoDiv}`)
--- hello_world.js
+++ hello_world.js
@@ -1,3 +1,13 @@
-let n = 3;
+//Escopo Local - Constante
+const i = 2;
+const j = 5;
 
-console.log(n);
+//Escopo Local/Global
+var n = 6;
+
+//Escopo Local
+let x = 8;
+
+const string1 = `Valor de ${n} + ${x} = ${x + n}`;
+console.log(string1);
+
